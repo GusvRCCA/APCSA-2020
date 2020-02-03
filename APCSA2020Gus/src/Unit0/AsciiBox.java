343073164b69256a670eb4bb1ded646ee1954451
gus <gus@10.0.1.199>
2020-02-02 23:26:11 -0800

    AsciiBox

---
+++ APCSA2020Gus/src/Unit0/AsciiBox.java
@@ -0,0 +1,22 @@
+package Unit0;
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Gus von Reis \t  Jan 30 \n\n" );
+		System.out.println("+++++++++++++++++++++++++\n"
+				+ "+++++++++++++++++++++++++\n"
+				+ "+++++++++++++++++++++++++");
+		
+		int n = 1;
+		while(n<=4){
+			System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA\n"
+					+ "AAAAAAAAAAAAAAAAAAAAAAAAA\n"
+					+"+++++++++++++++++++++++++\n"
+					+ "+++++++++++++++++++++++++\n"
+					+ "+++++++++++++++++++++++++");
+				    n++;
+		}
+	}
+}
